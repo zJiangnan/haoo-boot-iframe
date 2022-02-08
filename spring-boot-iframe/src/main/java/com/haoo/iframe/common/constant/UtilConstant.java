16162a133304e2f50e3605f1d7e0ae0374e0af28
haoo <784410011@qq.com>
2022-02-08 13:58:58 +0800

    格式化

--- spring-boot-iframe/src/main/java/com/haoo/iframe/common/constant/UtilConstant.java
+++ spring-boot-iframe/src/main/java/com/haoo/iframe/common/constant/UtilConstant.java
@@ -5,34 +5,21 @@ import java.util.Map;
 
 public interface UtilConstant {
 
-    /***
-     * 常规常量定义
-     */
+    //储存断点续传标识
+    Map<String, Boolean> MAP = new HashMap<>();
 
-    //以只读的方式打开文本，也就意味着不能用write来操作文件
-    String RANDOM_ACCESS_FILE_R = "r";
-    //读操作和写操作都是允许的
-    String RANDOM_ACCESS_FILE_RW = "rw";
     //每当进行写操作，同步的刷新到磁盘，刷新内容和元数据
     String RANDOM_ACCESS_FILE_RWS = "rws";
     //每当进行写操作，同步的刷新到磁盘，刷新内容
     String RANDOM_ACCESS_FILE_RWD = "rwd";
 
-    /**
-     * FileUtils 工具类常量定义
-     */
+    //以只读的方式打开文本，也就意味着不能用write来操作文件
+    String RANDOM_ACCESS_FILE_R = "r";
+    //读操作和写操作都是允许的
+    String RANDOM_ACCESS_FILE_RW = "rw";
 
-    // 上传文件拼接名称
+    //上传文件拼接名称
     String FILE_UTIL_SUFFIX_COPY = "_copy";
-
-    Map<String,Boolean> MAP = new HashMap<>();
-
-
-
-
-    /***
-     * OtherUtils 工具类类常量定义
-     */
 
 
 }
