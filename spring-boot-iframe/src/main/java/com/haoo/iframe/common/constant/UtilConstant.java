0471c2e08d65c3d3889f4831ac6c6685e0c255a5
haoo <784410011@qq.com>
2022-02-08 14:04:50 +0800

    格式化

--- spring-boot-iframe/src/main/java/com/haoo/iframe/common/constant/UtilConstant.java
+++ spring-boot-iframe/src/main/java/com/haoo/iframe/common/constant/UtilConstant.java
@@ -3,23 +3,23 @@ package com.haoo.iframe.common.constant;
 import java.util.HashMap;
 import java.util.Map;
 
-public interface UtilConstant {
+public class UtilConstant {
 
     //储存断点续传标识
-    Map<String, Boolean> MAP = new HashMap<>();
+    public final static Map<String, Boolean> MAP = new HashMap<>();
 
     //每当进行写操作，同步的刷新到磁盘，刷新内容和元数据
-    String RANDOM_ACCESS_FILE_RWS = "rws";
+    public final static String RANDOM_ACCESS_FILE_RWS = "rws";
     //每当进行写操作，同步的刷新到磁盘，刷新内容
-    String RANDOM_ACCESS_FILE_RWD = "rwd";
+    public final static String RANDOM_ACCESS_FILE_RWD = "rwd";
 
     //以只读的方式打开文本，也就意味着不能用write来操作文件
-    String RANDOM_ACCESS_FILE_R = "r";
+    public final static String RANDOM_ACCESS_FILE_R = "r";
     //读操作和写操作都是允许的
-    String RANDOM_ACCESS_FILE_RW = "rw";
+    public final static String RANDOM_ACCESS_FILE_RW = "rw";
 
     //上传文件拼接名称
-    String FILE_UTIL_SUFFIX_COPY = "_copy";
+    public final static String FILE_UTIL_SUFFIX_COPY = "_copy";
 
 
 }
